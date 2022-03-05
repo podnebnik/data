e15270cb8207262116b283142d7d03a369d691cc
Andraz Vrhovec <andraz@koofr.net>
2022-03-05 14:31:12 +0100

    Build datasette containers from data
    * reorganize file structure
    * add datasette and related libraries
    * add new tasks to build containers

--- tasks.py
+++ tasks.py
@@ -1,14 +1,48 @@
+#!/usr/bin/env python
+
 import os
 from invoke import task
 from pathlib import Path
 import zipfile
+import subprocess
+from datapackage_to_datasette import datapackage_to_datasette, DataImportError
 
+def build_container(dataset_name):
+    """ Build docker container from datasette dataset """
+    import subprocess
+    process = subprocess.Popen(['datasette', 'package', 'build/{}/data.db'.format(dataset_name), '-m', 'build/{}/metadata.json'.format(dataset_name), '-t', 'ghcr.io/podnebnik/data-{}:latest'.format(dataset_name)])
+    process.communicate()
+
+def package_dataset(dataset_name):
+    """ Package frictionless data dataset to datasette format """
+
+    try:
+        Path("build/{}".format(dataset_name)).mkdir(parents=True, exist_ok=True)
+        datapackage_to_datasette(
+            'build/{}/data.db'.format(dataset_name),
+            'dataset/{}/datapackage.yaml'.format(dataset_name),
+            'build/{}/metadata.json'.format(dataset_name),
+            write_mode='replace'
+        )
+    except DataImportError:
+        raise
+
+@task
+def package(c, dataset_name=None):
+    if dataset_name is None:
+        datasets = os.listdir("dataset")
+    else:
+        datasets = [dataset_name]
+    
+    for dataset in datasets:
+        package_dataset(dataset)
+        build_container(dataset)
 
 @task
 def zip(c, packageName):
     excludedFiles = [".DS_Store"]
     zip = zipfile.ZipFile("packages/{}.zip".format(packageName), mode="w")
-    for root, dirs, files in os.walk(packageName):
+    for root, dirs, files in os.walk('dataset/{}'.format(packageName)):
         if root == "{}/sources".format(packageName):  # exclude sources
             continue
         for file in files:
