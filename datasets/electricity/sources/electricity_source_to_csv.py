3a32fbfaaebc26fac40153c74d51a99522803a30
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2022-03-07 06:44:12 +0100

    Electricity: rename some fields

--- datasets/electricity/sources/electricity_source_to_csv.py
+++ datasets/electricity/sources/electricity_source_to_csv.py
@@ -28,19 +28,19 @@ if __name__ == '__main__':
         'Total [MW]': 'total', 
         'Thermal Power': 'thermal', 
         'Run-of-River hydro power': 'hydro',
-       'Mixed hydro power': 'mixed_hydro', 
-       'Pumped hydro power': 'pump_hydro', 
-       'Geothermal': 'geothermal', 
-       'Wind': 'wind',
-       'Solar thermal': 'solar_thermal', 
-       'Solar photovoltaic': 'solar', 
-       'Tide, wave, ocean': 'wave', 
-       'Nuclear': 'nuclear',
-       'Other fuels n.e.c.': 'other', 
-       'Consumption [TWh]': 'consumption'
+        'Mixed hydro power': 'mixed_hydro', 
+        'Pumped hydro power': 'pump_hydro', 
+        'Geothermal': 'geothermal', 
+        'Wind': 'wind',
+        'Solar thermal': 'solar_thermal', 
+        'Solar photovoltaic': 'solar', 
+        'Tide, wave, ocean': 'wave', 
+        'Nuclear': 'nuclear',
+        'Other fuels n.e.c.': 'other', 
+        'Consumption [TWh]': 'consumption'
         })
  
-    data.to_csv("..\data\electricity.installed_capacities.csv",index=False)    
+    data.to_csv("../data/electricity.installed_capacities.csv",index=False)    
     
     
     # electricity.additions_retirements
@@ -57,13 +57,13 @@ if __name__ == '__main__':
         'Wind.1': 'additions_wind', 
         'Solar photovoltaic.1': 'additions_solar', 
         'Run-of-River hydro power.1': 'additions_hydro',
-       'Pumped hydro power.1': 'additions_pump_hydro', 
-       'Nuclear.1': 'additions_nuclear', 
-       'Thermal Power.1': 'additions_thermal',
-       'Thermal Power Decommissions': 'retirements_thermal'
+        'Pumped hydro power.1': 'additions_pump_hydro', 
+        'Nuclear.1': 'additions_nuclear', 
+        'Thermal Power.1': 'additions_thermal',
+        'Thermal Power Decommissions': 'retirements_thermal'
         })
  
-    data.to_csv("..\data\electricity.additions_retirements.csv",index=False) 
+    data.to_csv("../data/electricity.additions_retirements.csv",index=False) 
     
     
     # electricity.emissions
@@ -75,8 +75,8 @@ if __name__ == '__main__':
     
     data = data[['Year', 'Total', 'Energetika Ljubljana', 'Energetika Maribor', 'TEB',
         'TEŠ', 'TETOL', 'TET', 'Energetika Celje',
-       'Enos', 'M-Energetika', 'Petrol Energetika', 'Other',
-       'Total all individual sources', 'Residual', '2030 Target']]
+        'Enos', 'M-Energetika', 'Petrol Energetika', 'Other',
+        'Total all individual sources', 'Residual', '2030 Target']]
     
     data = data.rename(columns = {
         'Year': 'year',
@@ -84,20 +84,20 @@ if __name__ == '__main__':
         'Energetika Ljubljana': 'energetika_ljubljana', 
         'Energetika Maribor': 'energetika_maribor', 
         'TEB': 'teb',
-       'TEŠ': 'test', 
-       'TETOL': 'tetol', 
-       'TET': 'tet', 
-       'Energetika Celje': 'energetika_celje',
-       'Enos': 'enos', 
-       'M-Energetika': 'm_energetika', 
-       'Petrol Energetika': 'petrol_energetika', 
-       'Other': 'other',
-       'Total all individual sources': 'total-individual', 
-       'Residual': 'residual', 
-       '2030 Target': 'target_2030'
+        'TEŠ': 'tes', 
+        'TETOL': 'tetol', 
+        'TET': 'tet', 
+        'Energetika Celje': 'energetika_celje',
+        'Enos': 'enos_energetika', 
+        'M-Energetika': 'm_energetika', 
+        'Petrol Energetika': 'petrol_energetika', 
+        'Other': 'other',
+        'Total all individual sources': 'total__individual', 
+        'Residual': 'total__residual', 
+        '2030 Target': 'target_2030'
         })
  
-    data.to_csv("..\data\electricity.emissions.csv",index=False) 
+    data.to_csv("../data/electricity.emissions.csv",index=False) 
     
     
     # electricity.hydro_units
